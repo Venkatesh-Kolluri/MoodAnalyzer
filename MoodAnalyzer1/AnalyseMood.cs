86f74c32a511b8c6165a1ed691eab84b44aa04c3
venky <venky94.vk@gmail.com>
2023-05-09 07:16:15 +0530

    [Venkatesh] File added

---
+++ MoodAnalyzer1/AnalyseMood.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzer1
+{
+    public class MoodAnalyser
+    {
+        public string message;
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+        public string AnalyseMood()
+        {
+            try
+            {
+                if (this.message.Equals(string.Empty))
+                {
+                    throw new MoodAnalyzerCustumException(MoodAnalyzerCustumException.ExceptionType.EMPTY_MESSAGE, "Mood should not be empty");
+                }
+                if (this.message.Contains("Sad"))
+                {
+                    return "SAD";
+                }
+                else
+                {
+                    return "HAPPY";
+                }
+            }
+            catch (NullReferenceException)
+            {
+                throw new MoodAnalyzerCustumException(MoodAnalyzerCustumException.ExceptionType.NULL_MESSAGE, "Mood should not be empty");
+            }
+            catch
+            {
+                return "HAPPY";
+            }
+
+
+        }
+    }
+}
---
+++ MoodAnalyzer1/UnitTest1.cs
@@ -0,0 +1,73 @@
+namespace MoodAnalyzer1
+{
+    public class Tests
+    {
+
+        [Test]
+        public void GivenSadMoodReturnSad()
+        {
+            //Arrange
+            string expected = "SAD";
+            string message = "I am in Sad Mood";
+            MoodAnalyser moodAnalyse = new MoodAnalyser(message);
+
+            //Act
+            string mood = moodAnalyse.AnalyseMood();
+
+            //Assert
+            Assert.AreEqual(expected, mood);
+        }
+        [Test]
+        public void GivenHappyMoodReturnHappy()
+        {
+            //Arrange
+            string expected = "HAPPY";
+            string message = "I am in Any Mood";
+            MoodAnalyser moodAnalyse = new MoodAnalyser(message);
+
+            //Act
+            string mood = moodAnalyse.AnalyseMood();
+
+            //Assert
+            Assert.AreEqual(expected, mood);
+        }
+        [Test]
+
+        public void GivenNullMoodReturnHappy()
+        {
+            //Arrange
+            string expected = "HAPPY";
+            string message = null;
+            MoodAnalyser moodAnalyse = new MoodAnalyser(message);
+
+            //Act
+            string mood = moodAnalyse.AnalyseMood();
+
+            //Assert
+            Assert.AreEqual(expected, mood);
+        }
+        [Test]
+
+        public void GivenNullMoodShouldThrowMoodAnalysisException()
+        {
+            //Arrange
+            try
+            {
+                //string expected = "HAPPY";
+                string message = "";
+                MoodAnalyser moodAnalyse = new MoodAnalyser(message);
+
+                //Act
+                string mood = moodAnalyse.AnalyseMood();
+            }
+            catch (MoodAnalyzerCustumException e)
+            {
+
+                //Assert
+                Assert.AreEqual("Mood should not be Empty", e.Message);
+            }
+        }
+      
+    }
+}
+        
